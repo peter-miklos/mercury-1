a619d094239e7f690a1a65da2f474312efebee79
Peter Miklos <pt.miklos@gmail.com>
2016-12-04 00:31:39 +0000

    fonts removed, defaultlayout updated

--- views/layouts/defaultLayout.jsx
+++ views/layouts/defaultLayout.jsx
@@ -7,10 +7,18 @@ class DefaultLayout extends React.Component {
         <head>
           <title>{this.props.title}</title>
           <link rel="stylesheet" href="/stylesheets/style.css" type="text/css" />
+          <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+          <link type="text/css" rel="stylesheet" href="stylesheets/materialize.css"  media="screen,projection"/>
           <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.8/js/materialize.min.js"></script>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </head>
-        <body>{this.props.children}</body>
+        <body>
+          {this.props.children}
+          <footer>
+          </footer>
+          <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+          <script type="text/javascript" src="javascripts/materialize.js"></script>
+        </body>
       </html>
     )
   }
