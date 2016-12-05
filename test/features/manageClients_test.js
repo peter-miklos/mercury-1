630141e48fd73bd0efc4f655f37d8b74580e1b72
Peter Miklos <pt.miklos@gmail.com>
2016-12-05 23:15:07 +0000

    new client related test cases added

--- test/features/manageClients_test.js
+++ test/features/manageClients_test.js
@@ -1,20 +1,20 @@
 process.env.NODE_ENV = 'test';
 
 var app = require('../../app');
-var Browser = require('zombie');
-var http = require('http');
-var mongoose = require('mongoose');
+    Browser = require('zombie');
+    http = require('http');
+    mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 describe("manage clients", function() {
-  server = http.createServer(app).listen(3000);
-  browser = new Browser({ site: 'http://localhost:3000'});
+  var server = http.createServer(app).listen(3000);
+  var browser = new Browser({ site: 'http://localhost:3000'});
 
   before(function() {
     mongoose.model("Client").remove({}, function(err) {
       console.log('collection removed')
     }).then(function() {
-      mongoose.model("Client").create({name: "Test User",
+      mongoose.model("Client").create({name: "Test Client",
                                        national_ID_number: "123456789AB",
                                        birth_date: "1979-11-11",
                                        birth_place: "London"
@@ -40,7 +40,40 @@ describe("manage clients", function() {
     })
 
     it("show the list of all clients", function() {
-      browser.assert.text("table", /Test User/)
+      browser.assert.text("table", /Test Client/)
     })
+  })
+
+  describe("add client", function() {
+    beforeEach(function(done) {
+      browser.visit('/').then(function() {
+        browser.clickLink("Add client", done);
+      })
+    })
+
+    beforeEach(function(done) {
+      browser
+        .fill('name', 'New Client')
+        .fill('national_ID_number', '987654321CC')
+        .fill('birth_date', '1982-03-09')
+        .fill('birth_place', 'Budapest, Hungary', done);
+    })
+
+    it("should be successfull", function() {
+      browser.assert.success();
+    })
+
+    it("shows the client's name in the client list", function() {
+      browser.assert.text("table", /New Client/)
+    })
+
+    it("shows the client's date of birth in the client list", function() {
+      browser.assert.text("table", /09-03-1982/)
+    })
+
+    it("shows the client's place of birth in the client list", function() {
+      browsert.assert.text("table", /Budapest, Hungary/)
+    })
+
   })
 })
