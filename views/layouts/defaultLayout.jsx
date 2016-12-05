7235782e639f4c9ee3641d03c11dc48a79b2f524
Peter Miklos <pt.miklos@gmail.com>
2016-12-05 12:25:17 +0000

    navbar/sidenav basics added

--- views/layouts/defaultLayout.jsx
+++ views/layouts/defaultLayout.jsx
@@ -1,5 +1,38 @@
 import React from 'react';
 
+class SideNav extends React.Component {
+  render() {
+    return (
+      <ul id="slide-out" className="side-nav fixed">
+        <li><div className="userView mainImage">
+          <div className="background" >
+            <img src="images/main_image.png" width="300"/>
+          </div>
+        </div></li>
+      <li><a href="#!"><i className="material-icons">cloud</i>First Link With Icon</a></li>
+        <li><a href="#!">Second Link</a></li>
+        <li><div className="divider"></div></li>
+        <li><a className="subheader">Subheader</a></li>
+        <li><a className="waves-effect" href="#!">Third Link With Waves</a></li>
+      </ul>
+    )
+  }
+}
+
+class Navbar extends React.Component {
+  render() {
+    return(
+      <nav className="light-blue lighten-2" role="navigtion">
+        <SideNav />
+        <a href="#" data-activates="slide-out" className="button-collapse"><i className="material-icons">menu</i></a>
+        <div className="nav-wrapper container">
+          <a href="#" className="brand-logo">mercury-1</a>
+        </div>
+      </nav>
+    )
+  }
+}
+
 class DefaultLayout extends React.Component {
   render() {
     return (
@@ -14,9 +47,15 @@ class DefaultLayout extends React.Component {
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         </head>
         <body>
-          {this.props.children}
+          <header>
+            <Navbar />
+          </header>
+          <main>
+            {this.props.children}
+          </main>
           <footer>
           </footer>
+          <script type="text/javascript" src="javascripts/feScripts.js"></script>
           <script type="text/javascript" src="javascripts/materialize.js"></script>
         </body>
       </html>
