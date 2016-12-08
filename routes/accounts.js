60187a586d1a9ed05337b42312fc49f8a78046e2
Peter Miklos <pt.miklos@gmail.com>
2016-12-08 19:39:48 +0000

    account routes and account/index view updated to show the client name in accounts list

--- routes/accounts.js
+++ routes/accounts.js
@@ -7,16 +7,9 @@ var express = require('express');
     Client = mongoose.model('Client');
 
 router.get('/', function(req, res, next) {
-  Account.find({}, function(accErr, accounts) {
-    if(accErr) {return handleError(accErr);}
-    else {
-      Client.find({}, '_id first_name last_name', function(clientErr, clients) {
-        if(clientErr) {return handleError(clientErr);}
-        else {
-          res.render('accounts/index', {title: "List of accounts", accounts: accounts, clients: clients});
-        }
-      })
-    }
+  Account.find({}).populate('_owner').exec(function(err, accounts) {
+    if(err) {return handleError(err);}
+    else {res.render('accounts/index', {title: "List of accounts", accounts: accounts});}
   })
 });
 
@@ -24,6 +17,18 @@ router.get('/new', function(req, res, next) {
   Client.getAllClientsInSortedList(function(err, clients) {
     if (err) {return handleError(err)}
     else { res.render('accounts/new', {title: "Add an account", clients: clients}); }
+  })
+})
+
+router.post("/create", function(req, res, next) {
+  Account.create({currency: req.body.ccySelection,
+                  _owner: req.body.clientSelection
+  }, function(accErr, account) {
+    if (accErr) {console.log(accErr)}
+    else {
+      console.log("New account has been successfully created")
+      res.redirect('/accounts');
+    }
   })
 })
 
